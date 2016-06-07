194e7622b9a59a403c68d4e61fe6bd28c571bbe5
Cecilia Le Brech <cecilialebrch@gmail.com>
2016-06-07 14:50:22 -0700

    Added initial files

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("form#celebrity").submit(function(event) {
+    var age = parseInt($("input#age").val());
+    var gender = $("select#gender").val();
+
+      if (gender === "male" && age < 21) {
+      $("#Miley").show();
+    } else if (gender === "male" && age >= 21 && age <= 30) {
+      ("#Scarlett").show();
+    } else if (gender === "male" && age > 30) {
+      $("#Hillary").show();
+    } else if(gender === "female") {
+      $("#Obama").show();
+
+    } else {
+      alert("Please enter information correctly. Refresh and try again!");
+    }
+    event.preventDefault();
+  });
+});
