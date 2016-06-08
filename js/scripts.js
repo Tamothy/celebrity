3e5ce821c6035f78f7495172e6fdfd84964122ae
Cecilia Le Brech <cecilialebrch@gmail.com>
2016-06-08 11:17:09 -0700

    Fixed html

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,14 @@ $(document).ready(function() {
     var age = parseInt($("input#age").val());
     var gender = $("select#gender").val();
 
+
       if (gender === "male" && age < 21) {
       $("#Miley").show();
     } else if (gender === "male" && age >= 21 && age <= 30) {
       $("#Scarlett").show();
     } else if (gender === "male" && age > 30) {
       $("#Hillary").show();
-    } else if(gender === "female") {
+    } else if (gender === "female") {
       $("#Obama").show();
     } else {
       alert("Please enter information correctly. Refresh and try again!");
